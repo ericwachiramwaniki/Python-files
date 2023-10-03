bc06000560122728c3103090291a00cf8c2a37e6
mwanikieric <91129232+ericwachiramwaniki@users.noreply.github.com>
2023-10-03 13:27:04 +0300

    Add files via upload

---
+++ Union of two sets.py
@@ -0,0 +1,13 @@
+# Define two sets
+set1 = {1, 2, 3, 4, 5}
+set2 = {4, 5, 6, 7, 8}
+
+# Using union() method
+union_set1 = set1.union(set2)
+
+# Using | operator
+union_set2 = set1 | set2
+
+# Print the union sets
+print("Union using union() method:", union_set1)
+print("Union using | operator:", union_set2)
