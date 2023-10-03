d1e09badce49b23c66edeaf060877c42f7ed28ef
mwanikieric <91129232+ericwachiramwaniki@users.noreply.github.com>
2023-10-03 13:24:37 +0300

    Add files via upload

---
+++ Intersection of Sets.py
@@ -0,0 +1,13 @@
+# Define two sets
+set1 = {1, 2, 3, 4, 5}
+set2 = {4, 5, 6, 7, 8}
+
+# Using intersection() method
+intersection_set1 = set1.intersection(set2)
+
+# Using & operator
+intersection_set2 = set1 & set2
+
+# Print the intersection sets
+print("Intersection using intersection() method:", intersection_set1)
+print("Intersection using & operator:", intersection_set2)
